92550de5ad0653336e9d75582a1ab59636e5ffbb
Beck-keleta <bereket.keleta20@gmail.com>
2023-07-11 00:00:49 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the num of args
+ * @argc: num of args
+ * @argv: array of args
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
